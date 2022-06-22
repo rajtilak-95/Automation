4463f88670f29e4e149820c5150398a3e5f59498
ratilak-95 <tilakraj.verulkar@gmail.com>
2022-06-22 18:24:21 +0530

    practice

---
+++ src/practice/Pyramid.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Pyramid {
+
+	public static void main(String[] args) {
+		System.out.println("*********");
+		for(int i=1;i<=5;i++) {
+			for(int j=5;j>=i;j--) 
+				System.out.print(" *");
+				System.out.println(" ");
+			}
+		}
+
+	}
+
+
